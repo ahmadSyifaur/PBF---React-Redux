6189512e9efe508232c287314e3067bcbeb08935
AhmadSyifaur <ahmadsyifaur11@gmail.com>
2020-03-19 15:51:22 +0700

    Redux Done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import MainReducer from './reducers/MainReducer' ;
 import './index.css';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.js'
 import * as serviceWorker from './serviceWorker';
+// import registerServiceWorker from './registerServiceWorker'; 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = compose(window.devToolsExtension ? window.devToolsExtension() : f => f)(createStore)(MainReducer) 
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+
+ReactDOM.render(<Provider store={store}>
+<App />
+</Provider>
+, document.getElementById('root'));
+// registerServiceWorker(); 
+
+// ReactDOM.render(
+// <App />, document.getElementById('root'));
+
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
